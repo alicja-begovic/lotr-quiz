fedf6465e26491f0a07720512c445be13f33f00d
Alicja Begović <alicja.begovic@gmail.com>
2024-04-18 19:39:07 +0200

    preparing questions

---
+++ src/questions.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    id: 'q1',
+    text: 'What is the name of the horse that rescues Aragorn in the Two Towers?',
+    answers: [
+      'Brego',
+      'Arot',
+      'Hasufel',
+      'Shadowfox',
+    ],
+  },
+  {
+    id: 'q2',
+    text: 'Which flower was growing mainly on graves and tombs in Rohan?',
+    answers: [
+      'Simbelmynë',
+      'Lissuin',
+      'Seregon',
+      'Uilos',
+    ],
+  },
+  {
+    id: 'q3',
+    text: 'What is a full name of J.R.R. Tolkien?',
+    answers: [
+      'John Ronald Reuel Tolkien',
+      'Jack Ronald Robert Tolkien',
+      'John Robert Ronald Tolkien',
+      'Jake Ronald Reuel Tolkien',
+    ],
+  },
+  {
+    id: 'q4',
+    text: 'What was the name of hair of Isildur?',
+    answers: [
+      'Aragorn',
+      'Boromir',
+      'Denethor',
+      'Faramir',
+    ],
+  },
+  {
+    id: 'q5',
+    text: 'What was the name of Orc, captain of Mordor who led a host of Orcs to join the Uruk-hai who were taking Merry and Pippin across Rohan to Isengard.?',
+    answers: [
+      'Grishnákh',
+      'Azog',
+      'Lug',
+      'Radbug',
+    ],
+  },
+  {
+    id: 'q6',
+    text: 'Who directed an animated version of The Lord of the Rings in 1978?',
+    answers: [
+      'Ralph Bakshi',
+      'Peter Jackson',
+      'Norbert Smith',
+      'Fans',
+    ],
+  },
+  {
+    id: 'q7',
+    text: 'How many days did it take to film the entire Lotr movie series?',
+    answers: [
+      '438',
+      '562',
+      '430',
+      'Unknown',
+    ],
+  },
+];
